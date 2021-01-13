0d9707487d86855f8e2daf2f487845df32d17d63
Makarov ilya <74796903+Bakl9@users.noreply.github.com>
2021-01-13 14:34:26 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+
+            let ilysha = {
+                age: 25,
+                name: 'Makarov',
+                ipg: 999,
+            };
+
+            delete ilysha.ipg
+        
+
+            alert( ilysha.age )
+            alert( ilysha.name )
+            alert( ilysha.ipg )
+            alert( ilysha.profile )
